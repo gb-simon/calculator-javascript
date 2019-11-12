12e3ed7003632988afe0a9f188c7059578626f32
Simon <aguilarsimon01@gmail.com>
2019-11-12 14:19:36 -0300

    I added the html, css and js docs

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const deleteButton = document.querySelector('[data-equals]')
+const allClearButton = document.querySelector('[data-delete]')
+const previousButton = document.querySelector('[data-all-clear]')
+const currrentButton = document.querySelector('[data-previous-operand]')
+const equalsButton = document.querySelector('[data-current-operand]')
+  
